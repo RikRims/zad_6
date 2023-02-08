e1268e95e04b8ecd1de8de6fb225b257fd8d3093
Rik Rims <Kirill.smirik1@ya.ru>
2023-02-08 17:34:40 +0300

    Добавьте файлы проекта.

---
+++ zad_6/Program.cs
@@ -0,0 +1,30 @@
+﻿//вывод таблицы умножения
+void Table (int n)
+{
+    for (int i = 0; i <= n; i++)
+    {
+        Console.Write($"| ");
+        for (int j = 0; j <= n; j++)
+        {
+            if (j == 0) Console.Write("");
+            else if (i == 0) Console.Write($"{j}  ");
+            if ((j == 0) ^ (i == 0)) Console.Write("");
+            else if (i * j < 10) Console.Write(" ");
+            if ((i > 0) & (j > 0)) Console.Write($"{i * j} ");
+        }
+        Console.WriteLine(" ");
+    }
+
+    
+}
+
+//читай текст ниже
+int n;
+Console.Write("введите число, до которого выводит таблицу умножения: ");
+while (!int.TryParse(Console.ReadLine(), out n))
+{
+    Console.WriteLine("Ошибка ввода. Дай число: ");
+}
+
+//вызов функции
+Table(n);
